de8cd3d43f4621411991db063d8d348b3e88d16d
Katherine Pham <katherinepham@Katherines-MacBook-Pro-2.local>
2022-09-03 14:57:58 -0700

    crying in purple

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const NUM_HEARTS = 50
 const NUM_TANGERINES = 7
 
-const player = document.querySelector('player')
+const player = document.querySelector('.player')
 const player_pos = {
     x: parseInt(window.innerWidth / 2),
     y: parseInt(window.innerHeight / 2)
